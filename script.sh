5730ab5c61357af5641de5976789f8cded9eaac4
José Carlos <carlosanchesm@hotmail.es>
2018-01-26 18:24:41 +0100

    edit script

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo cp -r /home/pi/patfinderweb/* /var/www/html
 cd /var/www/html
+sudo rm -rf *
+ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
+sudo cp -r /home/pi/patfinderweb/* /var/www/html
 sudo chmod 755 *
 printf "todo copiado!\n"
